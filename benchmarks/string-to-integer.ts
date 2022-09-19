e985bb5b006d30df4a4557fc7b8a5b488addc33d
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-09-19 17:51:02 +0900

    Add string-to-integer

---
+++ benchmarks/string-to-integer.ts
@@ -0,0 +1,26 @@
+import { setupBenchmark } from './utils'
+
+const num = '1234567890'
+
+function useParseInt() {
+  return parseInt(num, 10)
+}
+
+function useNumberConstructor() {
+  return Number(num)
+}
+
+function useUnaryPlus() {
+  return +num
+}
+
+function usePlus() {
+  return num + 0
+}
+
+setupBenchmark([
+  { name: 'parseInt()', fn: useParseInt },
+  { name: 'Number() constructor', fn: useNumberConstructor },
+  { name: 'Unary plus operator', fn: useUnaryPlus },
+  { name: 'Plus operator', fn: usePlus },
+]).run()
