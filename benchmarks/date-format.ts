0a0a8cefc0665c62aed5cd5915f3fb46acb36f1e
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2022-10-01 18:34:48 +0900

    Update date-format

--- benchmarks/date-format.ts
+++ benchmarks/date-format.ts
@@ -32,22 +32,21 @@ function useIntlDateTimeFormat() {
 }
 
 function useStringInterpolation() {
-  return `${date.getFullYear()}/${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')} ${date
-    .getHours()
-    .toString()
-    .padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date
-    .getSeconds()
-    .toString()
-    .padStart(2, '0')}`
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hour = date.getHours().toString().padStart(2, '0')
+  const minute = date.getMinutes().toString().padStart(2, '0')
+  const second = date.getSeconds().toString().padStart(2, '0')
+
+  return `${year}/${month}/${day} ${hour}:${minute}:${second}`
 }
 
-// console.log(useDateFns())
-// console.log(useDayjs())
-// console.log(useLuxon())
-// console.log(useIntlDateTimeFormat())
-// console.log(useStringInterpolation())
+console.log(useDateFns())
+console.log(useDayjs())
+console.log(useLuxon())
+console.log(useIntlDateTimeFormat())
+console.log(useStringInterpolation())
 
 export const suite = setupBenchmark([
   { name: 'date-fns', fn: useDateFns },
